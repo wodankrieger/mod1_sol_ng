14ac8290436fbdff5994fc649e86e66b5e17eaf7
Patrick Hoyer <patrick.hoyer@g6ye450001ss.lan.adytonsystems.com>
2016-09-19 11:11:02 +0200

    trimmed string input and added message function

--- app.js
+++ app.js
@@ -12,10 +12,22 @@
         $scope.checkButton = function () {
             var totalNameValue = wordCount($scope.name);
             $scope.totalValue = totalNameValue;
+
+                if ($scope.totalValue == 0) {
+                    $scope.message = "Please enter data first";
+                } else if ($scope.totalValue <= 3) {
+                    $scope.message = "Enjoy!";
+                } else $scope.message = "Too much!";
+
         };
 
+
         function wordCount(string) {
+            string.trim();
+            if (string === "") return 0;
             return string.split(',').length;
         }
+
+
     }
 })();
